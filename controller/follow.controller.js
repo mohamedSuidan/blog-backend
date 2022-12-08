f3dbe6231e3f1aabe97a05f051da6d87817ba5be
mohamedSuidan <msuidan149@gmail.com>
2022-12-08 01:21:44 -0800

    edit-follow

--- controller/follow.controller.js
+++ controller/follow.controller.js
@@ -3,38 +3,22 @@ exports.addFollow = async (req, res, next) => {
   let userAddFollow = await userModel.findById(req.body.followAdded);
   let userTakeFollow = await userModel.findById(req.body.takeFollow);
 
-  if (userAddFollow.following.length === 0) {
+  if (userAddFollow.following.includes(req.body.takeFollow)) {
+    await userModel.findByIdAndUpdate(req.body.followAdded, {
+      $pull: { following: req.body.takeFollow },
+    });
+  } else {
     await userModel.findByIdAndUpdate(req.body.followAdded, {
       $push: { following: req.body.takeFollow },
     });
-  } else {
-    userAddFollow.following.forEach(async (ele) => {
-      if (ele === req.body.takeFollow) {
-        await userModel.findByIdAndUpdate(req.body.followAdded, {
-          $pull: { following: req.body.takeFollow },
-        });
-      } else {
-        await userModel.findByIdAndUpdate(req.body.followAdded, {
-          $push: { following: req.body.takeFollow },
-        });
-      }
-    });
   }
-  if (userTakeFollow.followers.length === 0) {
+  if (userTakeFollow.followers.includes(req.body.followAdded)) {
+    await userModel.findByIdAndUpdate(req.body.takeFollow, {
+      $pull: { followers: req.body.followAdded },
+    });
+  } else {
     await userModel.findByIdAndUpdate(req.body.takeFollow, {
       $push: { followers: req.body.followAdded },
-    });
-  } else {
-    userTakeFollow.followers.forEach(async (ele) => {
-      if (ele === req.body.followAdded) {
-        await userModel.findByIdAndUpdate(req.body.takeFollow, {
-          $pull: { followers: req.body.followAdded },
-        });
-      } else {
-        await userModel.findByIdAndUpdate(req.body.takeFollow, {
-          $push: { followers: req.body.followAdded },
-        });
-      }
     });
   }
 };
