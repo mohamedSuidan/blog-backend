28cc5ecea86a610002d61bffacbbf91060911b4e
mohamedSuidan <msuidan149@gmail.com>
2022-12-08 05:50:12 -0800

    edit-profile

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -4,15 +4,10 @@ exports.getProfile = async (req, res, next) => {
   let { page = 1, limit = 3 } = req.query;
 
   let user = await userModel.findById(req.params.id);
-  let posts = await postModel
-    .find({ autherId: req.params.id })
-    .limit(limit * 1)
-    .skip((page - 1) * limit);
-  let len = (await postModel.find()).length;
+  let posts = await postModel.find({ autherId: req.params.id }).limit(2);
 
   res.json({
     user: user,
     posts: posts,
-    len: Math.ceil(len / limit),
   });
 };
